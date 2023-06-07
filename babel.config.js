c0184b7178470fd392804b1fea01b082767ae150
Dmitry Kvasnikov <dmitry.kvasnikov@gmail.com>
2023-06-07 14:47:22 +0200

    Core-JS fixed

--- babel.config.js
+++ babel.config.js
@@ -3,9 +3,12 @@ module.exports = {
     [
       "@babel/preset-env",
       {
-        useBuiltIns: "usage",
-        corejs: '3.30'
+        "useBuiltIns": "usage",
+        "corejs": "3.30",
+        "exclude": ["@babel/plugin-transform-regenerator", "@babel/plugin-transform-arrow-functions"]
+
       }
     ]
-  ]
+  ],
+  exclude: /node_modules/,
 }
